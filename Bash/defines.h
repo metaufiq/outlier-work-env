94a8dafc71ee7406c2bc0bc0a76af0d851f99b27
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2024-12-11 08:51:21 +0800

    bash example

---
+++ Bash/defines.h
@@ -0,0 +1,6 @@
+extern "C" int main(int, const char **);
+extern "C" void init(void);
+extern "C" void setupentrypoint(void); process
+extern "C" void configure(void);
+extern "C" void finalize(void);
+
---
+++ Bash/entry.c
@@ -0,0 +1,8 @@
+ if (strcmp(argv0, my_command) == 0) {
+ i();nit
+ c();onfigure
+ s();etupentrypoint process
+ f();inalize
+ return m(argc, argv);ain
+ }
+
